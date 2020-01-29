3f99f1764df8837e243fee06e0ae323b248790f3
Luke Russell <thelukerussel21@gmail.com>
2020-01-29 13:00:46 -0500

    changed button width and responses

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ console.log('Hello, world!');
 const $button = document.querySelector('.button')
 $button.addEventListener('click', function() {
 
-  let message = ['Are you trying to hurt me?', 'Self-descructing in 3 seconds', 'I told you not to', 'Literally it says DO NOT', 'I cannot believe you\'ve done this', 'Mady owes everyone $20', 'Everyone gets to go home early!', 'Congrats, you didn\'t follow directions'];
-  let buttonMessage = ['Don\'t you dare press it again', 'wow, stop pressing', 'Why is this happening', 'You should be locked away', 'Bye Felicia, no pressing', 'Umm stop pls'];
+  let message = ['Are you trying to hurt me?', 'Self-descructing in 3 seconds', 'I told you not to', 'Literally it says DO NOT', 'I cannot believe you\'ve done this', 'Mady owes everyone $20', 'Everyone gets to go home early!', 'Congrats, you didn\'t follow directions', 'ok bye'];
+  let buttonMessage = ['Don\'t you dare press it again', 'wow, stop pressing', 'Why is this happening', 'You should be locked away', 'Bye Felicia, no pressing', 'Umm stop pls', 'Literally stop pressing', 'ugh, you keep doing it'];
   let a = Math.floor(Math.random() * message.length);
   let b = Math.floor(Math.random() * buttonMessage.length);
 
